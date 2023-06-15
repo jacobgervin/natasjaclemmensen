4d4edfeae5d1310f373e219e1803f4f612f2ae50
jacobgervin <jacobgervin@gmail.com>
2023-06-15 19:48:28 +0200

    MULTIMEDIEDESIGNER

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -7,7 +7,7 @@ function Welcome({themeColor}) {
             <SvgComponent themeColor={themeColor} />
             </div>
             <div className="absolute z-10 md:bottom-20 bottom-10 text-center space-y-2">
-            <h2 className="font-ivyora font-thin text-lg md:text-2xl tracking-widest text-[#2A2538]">GRAFISK DESIGNER</h2>
+            <h2 className="font-ivyora font-thin text-lg md:text-2xl tracking-widest text-[#2A2538]">MULTIMEDIEDESIGNER</h2>
             <h1 className="font-ivyora font-medium text-xl md:text-3xl tracking-widest text-[#2A2538]">NATASJA CLEMMENSEN</h1>
             </div>
         </div>
